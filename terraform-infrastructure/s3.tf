d6c730946604893ae9c88744e6a1f4d315eb0de6
Naveed Akhtar <n6khtar@gmail.com>
2026-04-23 12:45:09 +0200

    Delete terraform-infrastructure directory

--- terraform-infrastructure/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "avatars" {
-  # Change this to something totally unique to you!
-  bucket = "grocerymate-avatars-naveed-12345"
-
-  tags = {
-    Name        = "grocerymate-avatars"
-    Environment = "Dev"
-  }
-}
